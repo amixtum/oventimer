d7c890a74f401b6633a6f2ec0935017aaf3b8ba0
Gabriel G. Parrott <ganiparrott@gmail.com>
2021-11-01 09:53:12 -0700

    scales

--- NoteHelper.py
+++ NoteHelper.py
@@ -5,9 +5,21 @@ class NoteHelper(object):
         self.center = center
         self.frequency = self.center
     
+    def setCenter(self, center):
+        self.center = center
+        self.frequency = center
+
     def transpose(self, semitones):
-        self.frequency *= (semitones * pow(2, 1 / 12))
+        if semitones < 0:
+            s = int(abs(semitones))
+            self.frequency *= ((1 / s) * pow(2, 1 / 12))
+        else:
+            self.frequency *= (semitones * pow(2, 1 / 12))
         return self.frequency
 
     def transposeFromCenter(self, semitones):
-        return self.center * (semitones * pow(2, 1 / 12))
+        if semitones < 0:
+            s = int(abs(semitones))
+            return self.center * ((1 / s) * pow(2, 1 / 12))
+        else:
+            return self.center * (semitones * pow(2, 1 / 12))
--- ScaleHelper.py
+++ ScaleHelper.py
@@ -1,2 +1,40 @@
 from NoteHelper import NoteHelper
 
+class ScaleHelper(object):
+    start = 0
+    fromStart = 0
+    notes = NoteHelper()
+    scale = [2, 2, 1, 2, 2, 2, 1]
+
+    def __init__(self, start=0, frequency=220):
+        self.start = start
+        self.notes.setCenter(frequency)
+        t = 0
+        if start > 0:
+            for _ in range(start):
+                t += self.scale[0]
+                self.scale.append(self.scale.pop(0))
+            self.notes.transpose(t)
+        else:
+            for _ in range(int(abs(start))):
+                t += self.scale[0]
+                self.scale.insert(0, self.scale.pop())
+            self.notes.transpose(-t)
+
+    def transpose(self, n):
+        self.fromStart += n
+        t = 0
+        if n > 0:
+            for _ in range(int(n)):
+                t += self.scale[0]
+                self.scale.append(self.scale.pop(0))
+            self.notes.transpose(t)
+        else:
+            for _ in range(int(abs(n))):
+                t += self.scale[0]
+                self.scale.insert(0, self.scale.pop())
+            self.notes.transpose(-t)
+        return self.notes.frequency
+
+    def frequency(self):
+        return self.notes.frequency
