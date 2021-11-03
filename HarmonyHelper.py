5cb8156c7045b3524a053ff85d8af95736f58e2f
Gabriel G. Parrott <ganiparrott@gmail.com>
2021-11-03 08:50:45 -0700

    experiment

--- HarmonyHelper.py
+++ HarmonyHelper.py
@@ -1,6 +1,9 @@
-from math import abs
+"""
+HarmonyHelper.py
+Help with identifying intervals and finding the transpositions required to make chords, etc.
+"""
 
-from PMNeuron import PMNeuron
+from math import abs
 
 def intervalFull(pm1, pm2):
     return pm1.intervalFull() - pm2.intervalFull()
@@ -8,8 +11,33 @@ def intervalFull(pm1, pm2):
 def interval(pm1, pm2):
     return pm1.interval() - pm2.interval()
 
+"""
+a = current interval
+x = transpose required to reach target interval
+a + x = target
+x = target - a
+"""
 def findTransposeFull(pm1, pm2, target):
     return (target - 1) - intervalFull(pm1, pm2)
 
 def findTranspose(pm1, pm2, target):
     return (target - 1) - interval(pm1, pm2)
+
+"""
+EXPERIMENTAL
+transposes pmResult based on the interval between pm1 and pm2
+"""
+def makeChord(pm1, pm2, pmResult):
+    i = abs(interval(pm1, pm2))
+    if i == 1: # second
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 5))
+    elif i == 2: # third
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 7))
+    elif i == 3: # fourth
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 6))
+    elif i == 4: # fifth
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 9)) 
+    elif i == 5: # sixth
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 5))
+    elif i == 6: # seventh
+        pmResult.transpose(findTransposeFull(pm2, pmResult, 3))
