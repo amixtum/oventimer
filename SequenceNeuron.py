b756d0adda404faa72b9b7e67a6d98ba1361eeb4
Gabriel G. Parrott <ganiparrott@gmail.com>
2021-11-04 07:39:43 -0700

    comment

--- SequenceNeuron.py
+++ SequenceNeuron.py
@@ -6,6 +6,13 @@ from PMNeuron import PMNeuron
 from RhythmGen import RhythmGen
 
 class SequenceNeuron(Neuron):
+    """
+        rootFrequency = the starting frequency of the first mode
+        bpm = tempo
+        mode = determines the starting frequency (see ScaleHelper.py)
+        sequence = the sequence of intervals to transpose the carrier frequency with
+        even, odd = the number of notes to be played on even or odd beats (see RhythmGen.py)
+    """
     def __init__(self, rootFrequency, bpm, mode, sequence, even=1, odd=1):
         self.bpm = bpm
         self.mode = mode
@@ -16,6 +23,9 @@ class SequenceNeuron(Neuron):
         self.sequence = sequence
         self.sidx = 0
     
+    """
+        queue the next note or rest, according to RhythmGen
+    """
     def fire(self):
         nxtFn = choice([self.rhythm.tickEighth, self.rhythm.tickSixteenth, self.rhythm.tickQuarter])
         nxt = nxtFn()
