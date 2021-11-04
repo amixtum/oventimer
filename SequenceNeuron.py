d16a6455daf717303ef4d91f0beb33f7b0d10f67
Gabriel G. Parrott <ganiparrott@gmail.com>
2021-11-04 07:35:33 -0700

    y

--- SequenceNeuron.py
+++ SequenceNeuron.py
@@ -17,28 +17,14 @@ class SequenceNeuron(Neuron):
         self.sidx = 0
     
     def fire(self):
-        nxtFn = [self.rhythm.tickEighth, self.rhythm.tickSixteenth, self.rhythm.tickQuarter]
-        nxtIdx = choice([0, 1, 2])
-        nxt = nxtFn[nxtIdx]()
+        nxtFn = choice([self.rhythm.tickEighth, self.rhythm.tickSixteenth, self.rhythm.tickQuarter])
+        nxt = nxtFn()
+        self.pm.setDuration(nxt[1])
         if nxt[0]:
-            if nxtIdx == 0:
-                self.pm.setEighth()
-            elif nxtIdx == 1:
-                self.pm.setSixteenth()
-            elif nxtIdx == 2:
-                self.pm.setQuarter()
-
             self.pm.queueSamples()
             self.pm.transpose(self.sequence[self.sidx])
             self.sidx = (self.sidx + 1) % len(self.sequence)
             if abs(self.pm.intervalFull()) >= 16:
                 self.pm.resetTranspose()
         else:
-            if nxtIdx == 0:
-                self.pm.setEighth()
-            elif nxtIdx == 1:
-                self.pm.setSixteenth()
-            elif nxtIdx == 2:
-                self.pm.setQuarter()
-            
             self.pm.queueRest()
